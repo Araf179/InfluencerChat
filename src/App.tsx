bed83d2068ddf822cb84ffe8d9c1be1b767c5a65
araf179 <arafshihab@outlook.com>
2021-12-27 14:47:30 -0500

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,70 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './styles/App.css';
 
-function App() {
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="ml-10 mt-5 md:grid md:justify-between md:grid-cols-3 gap-3">
+      <div className="max-w-sm rounded overflow-hidden shadow-lg">
+        <img className="object-contain h-48 w-96" src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/50/Logan_Paul_%2848086619418%29.jpg/1200px-Logan_Paul_%2848086619418%29.jpg" alt="Sunset in the mountains" />
+        <div className="flex flex-col justify-center items-center pricetag">$250</div>
+        <div className="px-6 py-4">
+          <div className="font-bold text-xl mb-2">Logan, Paul</div>
+          <div className="text-xl mb-2">Influencer Title</div>
+          <p className="text-gray-700 text-base">
+            Description
+          </p>
+        </div>
+        <div className="px-6 pt-4 pb-2">
+        <button className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded" >Pay to message</button>
+        <button className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">Pay to start a convo</button>
+        </div>
+      </div>
+      <div className="max-w-sm rounded overflow-hidden shadow-lg">
+        <img className="object-contain h-48 w-96" src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/50/Logan_Paul_%2848086619418%29.jpg/1200px-Logan_Paul_%2848086619418%29.jpg" alt="Sunset in the mountains" />
+        <div className="flex flex-col justify-center items-center pricetag">$250</div>
+        <div className="px-6 py-4">
+          <div className="font-bold text-xl mb-2">Logan, Paul</div>
+          <div className="text-xl mb-2">Influencer Title</div>
+          <p className="text-gray-700 text-base">
+            Description
+          </p>
+        </div>
+        <div className="px-6 pt-4 pb-2">
+        <button className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded" >Pay to message</button>
+        <button className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">Pay to start a convo</button>
+        </div>
+      </div>
+      <div className="max-w-sm rounded overflow-hidden shadow-lg">
+        <img className="object-contain h-48 w-96" src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/50/Logan_Paul_%2848086619418%29.jpg/1200px-Logan_Paul_%2848086619418%29.jpg" alt="Sunset in the mountains" />
+        <div className="flex flex-col justify-center items-center pricetag">$250</div>
+        <div className="px-6 py-4">
+          <div className="font-bold text-xl mb-2">Logan, Paul</div>
+          <div className="text-xl mb-2">Influencer Title</div>
+          <p className="text-gray-700 text-base">
+            Description
+          </p>
+        </div>
+        <div className="px-6 pt-4 pb-2">
+        <button className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded" >Pay to message</button>
+        <button className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">Pay to start a convo</button>
+        </div>
+      </div>
+      <div className="max-w-sm rounded overflow-hidden shadow-lg">
+        <img className="object-contain h-48 w-96" src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/50/Logan_Paul_%2848086619418%29.jpg/1200px-Logan_Paul_%2848086619418%29.jpg" alt="Sunset in the mountains" />
+        <div className="flex flex-col justify-center items-center pricetag">$250</div>
+        <div className="px-6 py-4">
+          <div className="font-bold text-xl mb-2">Logan, Paul</div>
+          <div className="text-xl mb-2">Influencer Title</div>
+          <p className="text-gray-700 text-base">
+            Description
+          </p>
+        </div>
+        <div className="px-6 pt-4 pb-2">
+        <button className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded" >Pay to message</button>
+        <button className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">Pay to start a convo</button>
+        </div>
+      </div>
+  </div>
   );
 }
 
